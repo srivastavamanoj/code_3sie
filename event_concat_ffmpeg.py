596b9497409b4b3cb93e5e80ab3beed03f182e3a
Manoj Srivastava <srivastavamanojk@gmail.com>
2020-07-10 10:05:56 -0700

    concatanating files; needed in ffmpeg video combining part

---
+++ event_concat_ffmpeg.py
@@ -0,0 +1,71 @@
+import time
+import os
+import glob
+
+dirname = '/home/mks/3sie/myCode/data/';
+lookBackCounter = 5 
+prefix1 = 'foo_'
+prefix2 = 'bar_'
+suffix = '.png'
+sep = '/'
+baseCmd1 = 'ffmpeg $options1 '
+baseCmd2 = 'gppeg $options2 '
+
+def create_one_file(fn):
+    open(fn,'a').close()
+
+def create_files():
+    tot=30;
+    idx =0;
+    while (idx<tot):
+        fn1 = dirname + prefix1 + str(idx) + suffix;
+        fn2 = dirname + prefix2 + str(idx) + suffix;
+        create_one_file(fn1)
+        create_one_file(fn2)
+        time.sleep(1)
+        #print('crated file: {}'.format(fn1))
+        idx+=1
+
+    return 0
+
+def get_index(s):
+    splitSep = '/'
+    word1 = s.split(splitSep)
+    #print('word1: {}'.format(word1))
+    word2 = word1[-1].split('.')
+    #print('word2: {}'.format(word2))
+    word3 = word2[0].split('_')
+    #print('word3: {}'.format(word3))
+    num = int(word3[1])
+    return num
+
+def find_latest():
+    searchDir = dirname +'*'
+    listFiles = glob.glob(searchDir)
+    latestFile = max(listFiles, key=os.path.getctime)
+    eventIdx = get_index(latestFile)
+    return eventIdx
+
+def get_cmd(baseCmd, prefix, eventIdx):
+    idx = 0
+    fileType = []
+    cmd = baseCmd
+
+    while idx < lookBackCounter:
+        actualIdx = eventIdx- lookBackCounter + 1 + idx
+        fn = dirname + prefix +  str(actualIdx) + suffix
+        #print ('filename: {}'.format(fn))
+        fileType.append(fn)
+        idx+=1
+
+    for l in fileType:
+        cmd += l + ' '
+    return cmd
+
+if __name__ == '__main__':
+    #create_files()
+    eventIdx = find_latest();
+    cmd1 = get_cmd (baseCmd1, prefix1, eventIdx)
+    cmd2 = get_cmd (baseCmd2, prefix2, eventIdx)
+    print ('cmd1: {}'.format(cmd1));
+    print ('cmd2: {}'.format(cmd2));
